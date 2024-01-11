63528fe270abbd5bf61c4745f81ff4a4e70ddd9e
Gregory Munro <minigsm14@gmail.com>
2024-01-11 17:50:09 +0100

    Added LogWindow, moved API Interface to seperate class

---
+++ AI-UI/LogWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace AI_UI
+{
+    /// <summary>
+    /// Interaction logic for LogWindow.xaml
+    /// </summary>
+    public partial class LogWindow : Window
+    {
+        public LogWindow() {
+            InitializeComponent();
+
+            LogBlock.Text = "Log Initialized";
+        }
+
+        public void WriteToLog(string output) {
+            LogBlock.Text = output + "\n" + LogBlock.Text;
+        }
+
+        //TODO: Save Log On Exit
+
+    }
+}
--- AI-UI/MainWindow.xaml.cs
+++ AI-UI/MainWindow.xaml.cs
@@ -1,109 +1,67 @@
 ﻿using System.Windows;
-using System.Net.Http;
-using System.Net.Http.Json;
 using System;
-using System.Text.Encodings.Web;
-using System.Text.Json;
-using System.Text.Unicode;
-using System.Diagnostics;
-using System.IO;
-using System.Windows.Threading;
-using System.ComponentModel;
 
 namespace AI_UI {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
-
-        struct requestStruct {
-            public string prompt { get; set; }
-            public string negative_prompt { get; set; }
-            public int seed { get; set; }
-            public int steps { get; set; }
-            public int width { get; set; }
-            public int height { get; set; }
-            public int batch_size { get; set; }
-        }
-
-        struct responseStruct {
-            public string[] images { get; set; }
-        }
-
-        struct progressStruct {
-            public double progress;
-        }
+        
+        //Reference to Loggin Window
+        LogWindow log;
 
         public MainWindow() {
             InitializeComponent();
 
+            //Create and Open Log Window
+            log = new();
+            log.Show();
+
+            //Initialize Stable Diffusion Interface
+            StableInterface.Initialize(this, log);
+
             ChangeStatusMessage("Ready...");
         }
 
+        /// <summary>
+        /// Called when Steps Slider is moved
+        /// </summary>
         void StepsSlider_Changed(object sender, RoutedPropertyChangedEventArgs<double> e) {
             StepsText.Text = "Steps: " + e.NewValue.ToString();
         }
 
+        /// <summary>
+        /// Called when Batch Size Slider is moved
+        /// </summary>
         void BatchSizeSlider_Changed(object sender, RoutedPropertyChangedEventArgs<double> e) {
             BatchSizeText.Text = "Batch Size: " + e.NewValue.ToString();
         }
 
+        /// <summary>
+        /// Called when Generate Button is clicked
+        /// </summary>
         void Generate_Click(object sender, RoutedEventArgs e) {
-            BackgroundWorker worker = new BackgroundWorker();
-            worker.DoWork += Generate;
-            worker.RunWorkerAsync(worker);
-            
+            GenerateButton.IsEnabled = false;
+
+            StableInterface.Generate(PromptBox.Text, NegativePromptBox.Text, Convert.ToInt32(SeedBox.Text), (int)StepsSlider.Value, (int)BatchSizeSlider.Value,
+                Convert.ToInt32(WidthBox.Text), Convert.ToInt32(HeightBox.Text));
         }
 
-        async void Generate(object sender, DoWorkEventArgs e) {
-            requestStruct request = new();
-
-            Dispatcher.Invoke(() => {
-                request.prompt = PromptBox.Text;
-                request.negative_prompt = NegativePromptBox.Text;
-                request.seed = Convert.ToInt32(SeedBox.Text);
-                request.steps = (int)StepsSlider.Value;
-                request.batch_size = (int)BatchSizeSlider.Value;
-                request.width = Convert.ToInt32(WidthBox.Text);
-                request.height = Convert.ToInt32(HeightBox.Text);
-            });
-
-            var jsonOptions = new JsonSerializerOptions() {
-                WriteIndented = true,
-                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
-            };
-            JsonContent requestBody = JsonContent.Create(request, request.GetType(), options: jsonOptions);
-
-            HttpClient client = new();
-            client.DefaultRequestHeaders.Clear();
-            client.DefaultRequestHeaders.Add("Accept", "application/json");
-            MessageBox.Show(await requestBody.ReadAsStringAsync(), "Request Body");
-            ChangeStatusMessage("Sending Request");
-            var result = await client.PostAsync("http://127.0.0.1:7860/sdapi/v1/txt2img", requestBody);
-            ChangeStatusMessage("Request Finished with " + (int)result.StatusCode + " " + result.StatusCode);
-            Console.WriteLine(result.ReasonPhrase);
-            string responseBody = await result.Content.ReadAsStringAsync();
-
-            responseStruct response = JsonSerializer.Deserialize<responseStruct>(responseBody);
-
-            if (response.images != null)
-                SaveImage(response.images);
-            else MessageBox.Show("Error", "No Images Found in Response");
+        /// <summary>
+        /// Change the Message visible in the Status Bar
+        /// </summary>
+        /// <param name="value">New Message to be displaye</param>
+        public void ChangeStatusMessage(string value) {
+            StatusText.Text = value;
         }
 
-        static void SaveImage(string[] base64string) {
-            string filePath = $"output\\" + DateTime.Now.ToString("dd.MM.yyyy hh-mm-ss") + "\\";
-            Directory.CreateDirectory(filePath);
-            int i = 0;
-            foreach (var image in base64string)
-                File.WriteAllBytes(filePath + i++ + ".png", Convert.FromBase64String(image));
-            Process.Start("explorer.exe", filePath);
+        /// <summary>
+        /// Change Progressbar Progress
+        /// </summary>
+        /// <param name="value">New Value of the Progressbar Progress</param>
+        public void ChangeProgressbar(double value) {
+            ProgressBar.Value = value;
         }
-
-        public void ChangeStatusMessage(string output) {
-            Dispatcher.Invoke(() =>
-            StatusText.Text = output
-            );
-        }
+        
     }
 }
