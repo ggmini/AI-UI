a4965e73df041cc28f0265fb1e9b3ad33d8f36ef
Gregory Munro <minigsm14@gmail.com>
2024-01-12 10:00:05 +0100

    Log is now saved to txt file

--- AI-UI/LogWindow.xaml.cs
+++ AI-UI/LogWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.IO;
+using System;
+using System.Windows;
 
 namespace AI_UI
 {
@@ -10,14 +13,25 @@ namespace AI_UI
         public LogWindow() {
             InitializeComponent();
 
-            LogBlock.Text = "Log Initialized";
+            LogBlock.Text = $"[{DateTime.Now.ToString("hh-mm-ss")}] Log Initialized";
         }
 
+        /// <summary>
+        /// Write a string to the Log
+        /// </summary>
+        /// <param name="output">String to write to log</param>
         public void WriteToLog(string output) {
-            LogBlock.Text = output + "\n" + LogBlock.Text;
+            LogBlock.Text = $"[{DateTime.Now.ToString("hh-mm-ss")}] {output}\n{LogBlock.Text}";
         }
 
-        //TODO: Save Log On Exit
+        /// <summary>
+        /// Save the Log to a txt file
+        /// </summary>
+        public void SaveLog() {
+            string filePath = $"log\\{DateTime.Now.ToString("dd.MM.yyyy hh-mm-ss")}.txt";
+            Directory.CreateDirectory("log");
+            File.WriteAllBytes(filePath, Convert.FromBase64String(LogBlock.Text));
+        }
 
     }
 }
