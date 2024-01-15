51ece3e5d40b9084efea8a759aa8d9953d68ce38
tk-spinat <tobbskrapf@gmail.com>
2024-01-15 13:13:26 +0100

    created template for creating visual tree usiung old ImgTree

---
+++ WpfBaum/WpfBaum/ImgTree.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfBaum
+{
+    class ImgTree
+    {
+        public class ImgItem
+        {
+            public string promptInfo;
+            public int id;
+        }
+
+        public class ImgNode
+        {
+            public int nodeId;
+            public string promptInfoOfNode;
+            public List<ImgItem> itemList;
+            public List<ImgNode> children;
+
+            public ImgNode(int nodeId)
+            {
+                this.nodeId = nodeId;
+                itemList = new List<ImgItem>();
+                children = new List<ImgNode>();
+            }
+        }
+
+        private ImgNode start = null;
+        private int countNodeId = 0;
+        private ImgNode currentNode = null;
+
+        public List<ImgNode> nodeList = new List<ImgNode>();
+
+        public ImgNode SelectNode(int pos)// set via onClick
+        {
+            if (currentNode != null && pos >= 0 && pos < nodeList.Count)
+            {
+                currentNode = nodeList[pos];
+                return currentNode;
+            }
+
+            return null;
+        }
+
+
+        public void InsertItem(string PromptInfo, int Id)
+        {
+            ImgItem neu = new ImgItem() { promptInfo = PromptInfo, id = Id };
+
+            // Erstes Element in Baum
+            if (start == null)
+            {
+                ImgNode newNode = new ImgNode(countNodeId++);
+                nodeList.Add(newNode);
+                start = newNode;
+                currentNode = newNode;
+                currentNode.promptInfoOfNode = PromptInfo;
+                currentNode.itemList.Add(neu);
+            }
+            else if (currentNode.promptInfoOfNode == PromptInfo) // von aktueller node
+            {
+                currentNode.itemList.Add(neu);
+            }
+            else
+            {
+                ImgNode newNode = new ImgNode(countNodeId++) { promptInfoOfNode = PromptInfo };
+                nodeList.Add(newNode);
+                currentNode.children.Add(newNode);
+
+                currentNode = newNode;
+                currentNode.itemList.Add(neu);
+            }
+        }
+
+        public void PrintTree()
+        {
+            PrintNode(start, "");
+        }
+
+        private void PrintNode(ImgNode node, string indent, bool last = true)
+        {
+            Console.Write(indent);
+
+            if (last)
+            {
+                Console.Write("\\-");
+                indent += "  ";
+            }
+            else
+            {
+                Console.Write("|-");
+                indent += "| ";
+            }
+
+            Console.WriteLine($"Node {node.nodeId}");
+
+            foreach (var item in node.itemList)
+            {
+                Console.WriteLine($"{indent}  Item {item.id}");
+            }
+
+            for (int i = 0; i < node.children.Count; i++)
+            {
+                bool isLastChild = i == node.children.Count - 1;
+                PrintNode(node.children[i], indent, isLastChild);
+            }
+        }
+    }
+}
---
+++ WpfBaum/WpfBaum/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace WpfBaum
+{
+    public partial class MainWindow : Window
+    {
+        public class TreeViewItemModel
+        {
+            public string Text { get; set; }
+            public ObservableCollection<TreeViewItemModel> Children { get; set; }
+
+            public TreeViewItemModel()
+            {
+                Children = new ObservableCollection<TreeViewItemModel>();
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Hier die Methode einsetzen, um aus Ihrem ImgTree-Objekt ein passendes Datenmodell zu erstellen
+            ImgTree imgTree = new ImgTree();
+            imgTree.InsertItem("Bär", 1);
+            imgTree.InsertItem("Bär", 2);
+            imgTree.InsertItem("Bär", 3);
+            imgTree.InsertItem("Bär fotorealistisch", 4);
+            imgTree.SelectNode(0);
+            imgTree.InsertItem("Bär", 5);
+            imgTree.SelectNode(1);
+            imgTree.InsertItem("Bär", 6);
+            imgTree.InsertItem("Bär", 7);
+            imgTree.InsertItem("Bär fotorealistisch", 8);
+
+            // Hier wird die Methode PopulateTreeView verwendet, um die TreeView zu füllen
+            PopulateTreeView(imgTree);
+        }
+
+        //private ImgTree CreateImgTree()
+        //{
+        //    // Hier den ImgTree mit den entsprechenden Daten erstellen
+        //    ImgTree imgTree = new ImgTree();
+
+
+        //    imgTree.InsertItem("Bär", 1);
+        //    imgTree.InsertItem("Bär", 2);
+        //    imgTree.InsertItem("Bär", 3);
+        //    imgTree.InsertItem("Bär fotorealistisch", 4);
+        //    imgTree.SelectNode(0);
+        //    imgTree.InsertItem("Bär", 5);
+        //    imgTree.SelectNode(1);
+        //    imgTree.InsertItem("Bär", 6);
+        //    imgTree.InsertItem("Bär", 7);
+        //    imgTree.InsertItem("Bär fotorealistisch", 8);
+
+        //    PopulateTreeView();
+
+
+        //    // Fügen Sie Elemente zum ImgTree hinzu, wie im vorherigen Beispiel gezeigt
+        //    return imgTree;
+        //}
+
+        private void PopulateTreeView(ImgTree imgTree)
+        {
+            treeView.Items.Clear();
+
+            List<ImgTree.ImgNode> addedNodes = new List<ImgTree.ImgNode>();
+
+            foreach (var node in imgTree.nodeList)
+            {
+                if (!addedNodes.Contains(node))
+                {
+                    TreeViewItem treeNode = CreateTreeViewItem(node, addedNodes);
+                    treeView.Items.Add(treeNode);
+                }
+            }
+        }
+
+        private TreeViewItem CreateTreeViewItem(ImgTree.ImgNode node, List<ImgTree.ImgNode> addedNodes)
+        {
+            TreeViewItem treeNode = new TreeViewItem { Header = $"Node {node.nodeId}" };
+
+            addedNodes.Add(node);
+
+            foreach (var item in node.itemList)
+            {
+                treeNode.Items.Add(new TreeViewItem { Header = $"Item {item.id}" });
+            }
+
+            foreach (var childNode in node.children)
+            {
+                TreeViewItem childTreeNode = CreateTreeViewItem(childNode, addedNodes);
+                treeNode.Items.Add(childTreeNode);
+            }
+
+            return treeNode;
+        }
+
+        private ObservableCollection<TreeViewItemModel> CreateTreeViewData(ImgTree imgTree)
+        {
+            ObservableCollection<TreeViewItemModel> treeViewData = new ObservableCollection<TreeViewItemModel>();
+
+            // Hier die Methode einsetzen, um aus Ihrem ImgTree-Objekt ein passendes Datenmodell zu erstellen
+            // Sie müssen durch die Hierarchie des ImgTree navigieren und TreeViewItemModel-Objekte erstellen
+            // Hier ein Platzhalter-Code:
+            foreach (var imgNode in imgTree.nodeList)
+            {
+                TreeViewItemModel nodeModel = new TreeViewItemModel { Text = $"Node {imgNode.nodeId}" };
+                foreach (var item in imgNode.itemList)
+                {
+                    nodeModel.Children.Add(new TreeViewItemModel { Text = $"Item {item.id}" });
+                }
+                treeViewData.Add(nodeModel);
+            }
+
+            return treeViewData;
+        }
+    }
+}
